383b08444dab55d3c626f295f3c7e0e6b6b92623
zhaoamy <zhaoamy123@gmail.com>
2017-11-01 01:52:24 -0400

    data

--- data.py
+++ data.py
@@ -1,6 +1,17 @@
 import csv
 
-open("airbnb-public-sep-2017/airbnb-sep-2017/listings.csv/listings.csv")
+listing = open("airbnb-public-sep-2017/airbnb-sep-2017/listings.csv/listings.csv")
+if listing.close
+    print "not here"
+
+if true
+    print "here"
+
+csvReader = csv.reader(listing)
 
 
-print "hello"
+for row in csvReader
+    print row
+
+
+print "goodbye"
