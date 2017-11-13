121b44ba9cac8762d742f79358104b90d2855aca
zhaoamy <zhaoamy123@gmail.com>
2017-11-12 22:37:09 -0500

    front end

--- app.js
+++ app.js
@@ -20,6 +20,11 @@ function readTextFile(file, callback) {
     rawFile.send(null);
 }
 
+function buttonClick() {
+  calculateAverageIncome();
+  findIdealPrice();
+}
+
 function calculateAverageIncome() {
     /*get latitude and longitude*/
     var data;
@@ -30,14 +35,16 @@ function calculateAverageIncome() {
         data = JSON.parse(text);
 
     });
+
     var user_long = parseFloat(document.getElementById("longitude").value);
     var user_lat = parseFloat(document.getElementById("latitude").value);
-    console.log(user_long);
-    console.log(user_lat);
+
     if ( (user_long > -122.36475849999999) ||
         (user_long < -122.5115) || (user_lat < 37.706927690000001) ||
          (user_lat > 37.83109279) ) {
           window.alert("Invalid Coordinates! Must be in San Francisco.");
+          document.getElementById("average").innerHTML = "";
+
     }
     else {
 
@@ -46,23 +53,15 @@ function calculateAverageIncome() {
 
 
         for (i = 0; i < 8706; i++) {
-          latitude.push(parseFloat(data['latitude'][i]));
-          longitude.push(parseFloat(data['longitude'][i]));
-          prices.push(parseFloat(data['price'][i]));
+            latitude.push(parseFloat(data['latitude'][i]));
+            longitude.push(parseFloat(data['longitude'][i]));
+            prices.push(parseFloat(data['price'][i]));
         }
 
-        console.log(data);
-        console.log(prices);
-        console.log(user_lat - .1);
-        console.log(user_long - .1);
-        console.log(user_lat + .1);
-        console.log(user_long + .1);
-        console.log(latitude[1]);
-        console.log(longitude[1]);
+
         for (i = 0; i < 8706; i++) {
           if ((latitude[i] >= (user_lat - .01)) && (latitude[i] <= (user_lat + .01))
               && (longitude[i] >= (user_long - .01)) && (longitude[i] <= (user_long + .01))) {
-                console.log("here");
                 sum =  sum + prices[i];
                 num_sum++;
           }
@@ -74,84 +73,80 @@ function calculateAverageIncome() {
         else {
           average = (sum/num_sum)*7;
 
-          document.getElementById("average").innerHTML = average;
+          document.getElementById("average").innerHTML = "You will earn an average of: $" + (Math.round(average * 100) / 100) + " per week.";
 
         }
-
-
-        console.log(average);
     }
 }
 
 function findIdealPrice() {
-  var data;
-  readTextFile("airbnb-public-sep-2017/airbnb-sep-2017/optimal.json", function(text){
-      data = JSON.parse(text);
 
-  });
+    var data;
+    readTextFile("airbnb-public-sep-2017/airbnb-sep-2017/optimal.json", function(text){
+        data = JSON.parse(text);
 
-  var user_long = parseFloat(document.getElementById("longitude").value);
-  var user_lat = parseFloat(document.getElementById("latitude").value);
+    });
 
-  var i, sum = 0, num_sum = 0, average=0, latitude = [], longitude = [], prices = [],
-      num_reviews= [], ratings = [];
-  var score;
-  var top_score = [0,0,0];
-  var top_price = [0,0,0];
+    var user_long = parseFloat(document.getElementById("longitude").value);
+    var user_lat = parseFloat(document.getElementById("latitude").value);
 
+    if ( (user_long > -122.36475849999999) ||
+        (user_long < -122.5115) || (user_lat < 37.706927690000001) ||
+         (user_lat > 37.83109279) ) {
+          document.getElementById("content").innerHTML = "";
 
-
-  for (i = 0; i < 8706; i++) {
-      latitude.push(parseFloat(data['latitude'][i]));
-      longitude.push(parseFloat(data['longitude'][i]));
-      prices.push(parseFloat(data['price'][i]));
-      num_reviews.push(parseInt(data['num_reviews'][i]));
-      if (num_reviews[i] == 0) {
-        ratings.push(0);
-      }
-      else {
-        ratings.push(parseInt(data['review_score'][i]));
     }
-  }
+    else {
+          var i, sum = 0, num_sum = 0, average=0, latitude = [], longitude = [], prices = [],
+              num_reviews= [], ratings = [];
+          var score;
+          var top_score = [0,0,0];
+          var top_price = [0,0,0];
 
-  for (i = 0; i < 8706; i++) {
-    if ((latitude[i] >= (user_lat - .01)) && (latitude[i] <= (user_lat + .01))
-        && (longitude[i] >= (user_long - .01)) && (longitude[i] <= (user_long + .01))) {
-          if(num_reviews[i] != 0) {
-            console.log("hi");
-            console.log(num_reviews[i]);
-            console.log(ratings[i]);
-              score = num_reviews[i] * ratings[i];
-              console.log(score);
-              if (score > top_score[0] && score < top_score[1] && score< top_score[2]) {
-                console.log("here");
-                  top_score[0] = score;
-                  top_price[0] = prices[i];
-              }
-              else if(score > top_score[0] && score > top_score[1] && score < top_score[2]) {
-                console.log("there");
-                  top_score[1] = score;
-                  top_price[1] = prices[i];
-              }
-              else if(score > top_score[0] && score > top_score[1] && score > top_score[2]) {
-                console.log("everywhere");
-                top_score[2] = score;
-                  top_price[2] = prices[i];
-              }
 
+
+          for (i = 0; i < 8706; i++) {
+              latitude.push(parseFloat(data['latitude'][i]));
+              longitude.push(parseFloat(data['longitude'][i]));
+              prices.push(parseFloat(data['price'][i]));
+              num_reviews.push(parseInt(data['num_reviews'][i]));
+              if (num_reviews[i] == 0) {
+                ratings.push(0);
+              }
+              else {
+                ratings.push(parseInt(data['review_score'][i]));
+            }
           }
+
+          for (i = 0; i < 8706; i++) {
+            if ((latitude[i] >= (user_lat - .01)) && (latitude[i] <= (user_lat + .01))
+                && (longitude[i] >= (user_long - .01)) && (longitude[i] <= (user_long + .01))) {
+                  if(num_reviews[i] != 0) {
+
+                      score = num_reviews[i] * ratings[i];
+
+                      if (score > top_score[0] && score < top_score[1] && score< top_score[2]) {
+                          top_score[0] = score;
+                          top_price[0] = prices[i];
+                      }
+                      else if(score > top_score[0] && score > top_score[1] && score < top_score[2]) {
+                          top_score[1] = score;
+                          top_price[1] = prices[i];
+                      }
+                      else if(score > top_score[0] && score > top_score[1] && score > top_score[2]) {
+                        top_score[2] = score;
+                          top_price[2] = prices[i];
+                      }
+
+                  }
+            }
     }
 
 }
-console.log(top_price[0]);
-console.log(top_price[1]);
-console.log(top_price[2]);
-console.log(top_score[0]);
-console.log(top_score[1]);
-console.log(top_score[2]);
-var optimal = (top_price[0] + top_price[1] + top_price[2]) / 3;
-document.getElementById("content").innerHTML = optimal;
-console.log(optimal);
+
+  var optimal = (top_price[0] + top_price[1] + top_price[2]) / 3;
+  document.getElementById("content").innerHTML = "The optimal price per night based on location is: $" + (Math.round(optimal * 100) / 100);
+
 }
 
 function createGraph1() {
@@ -161,13 +156,17 @@ function createGraph1() {
       "renderAt": "chartContainer",
       "width": "1350",
       "height": "500",
-      "chartLeftMargin" :"100",
       "dataFormat": "json",
       "dataSource": {
         "chart": {
             "caption": "Popularity of Neighborhood Listings",
             "xAxisName": "Neighborhoods",
             "yAxisName": "Number of Listings",
+            "xAxisNameBorderPadding": "100",
+            "xAxisNameFontSize": "15",
+            "yAxisNameFontSize": "15",
+            "baseFontSize" : "13",
+            "labelFontBold" : "1",
             "theme": "fint",
             "labelDisplay": "rotate",
             "slantLabels": "1"
@@ -192,7 +191,6 @@ function createGraph2() {
         dataSource: {
             "chart": {
                 "caption": "Number of Amenities Offered",
-                "subCaption": "Last year",
                 "paletteColors": "#0075c2,#1aaf5d,#f2c500,#f45b00,#8e0000",
                 "chartLeftMargin": "100",
                 "bgColor": "#ffffff",
@@ -203,6 +201,8 @@ function createGraph2() {
                 "startingAngle": "0",
                 "showPercentValues": "1",
                 "showPercentInTooltip": "0",
+                "xAxisNameFontSize": "15",
+                "yAxisNameFontSize": "15",
                 "decimals": "1",
                 "captionFontSize": "14",
                 "subcaptionFontSize": "14",
@@ -251,12 +251,13 @@ function createGraph3() {
     dataSource: {
         "chart": {
             "caption": "Number of Amenities vs Number of Reviews",
-            "subCaption": "Last month",
             "yAxisName": "Number of Reviews",
             "xAxisName": "Number of Amenities",
             "paletteColors": "#0075c2",
             "bgColor": "#ffffff",
             "showBorder": "0",
+            "xAxisNameFontSize": "15",
+            "yAxisNameFontSize": "15",
             "showCanvasBorder": "0",
             "usePlotGradientColor": "0",
             "plotBorderAlpha": "10",
@@ -275,9 +276,10 @@ function createGraph3() {
             "toolTipBgColor": "#000000",
             "toolTipBgAlpha": "80",
             "toolTipBorderRadius": "2",
-            "toolTipPadding": "5"
+            "toolTipPadding": "5",
+            "valuePadding": "10"
         },
-        "data": [{"label":"1","value":2038.0},{"label":"2","value":468.0},{"label":"3","value":557.0},{"label":"4","value":1438.0},{"label":"5","value":1226.0},{"label":"6","value":2047.0},{"label":"7","value":3176.0},{"label":"8","value":4556.0},{"label":"9","value":6894.0},{"label":"10","value":8594.0},{"label":"11","value":9504.0},{"label":"12","value":12131.0},{"label":"13","value":13918.0},{"label":"14","value":14131.0},{"label":"15","value":15435.0},{"label":"16","value":16330.0},{"label":"17","value":17360.0},{"label":"18","value":14526.0},{"label":"19","value":14943.0},{"label":"20","value":13671.0},{"label":"21","value":12764.0},{"label":"22","value":11093.0},{"label":"23","value":6573.0},{"label":"24","value":5595.0},{"label":"25","value":3189.0},{"label":"26","value":1757.0},{"label":"27","value":1031.0},{"label":"28","value":729.0},{"label":"29","value":255.0},{"label":"30","value":258.0},{"label":"31","value":129.0},{"label":"32","value":6.0},{"label":"33","value":0.0},{"label":"34","value":6.0},{"label":"35","value":27.0},{"label":"42","value":14.0}]
+        "data": [{"label":"1","value":2038.0, "color": "#1aaf5d"},{"label":"2","value":468.0, "color": "#1aaf5d"},{"label":"3","value":557.0, "color": "#1aaf5d"},{"label":"4","value":1438.0, "color": "#1aaf5d"},{"label":"5","value":1226.0, "color": "#1aaf5d"},{"label":"6","value":2047.0, "color": "#1aaf5d"},{"label":"7","value":3176.0, "color": "#1aaf5d"},{"label":"8","value":4556.0, "color": "#1aaf5d"},{"label":"9","value":6894.0, "color": "#1aaf5d"},{"label":"10","value":8594.0, "color": "#1aaf5d"},{"label":"11","value":9504.0, "color": "#1aaf5d"},{"label":"12","value":12131.0, "color": "#1aaf5d"},{"label":"13","value":13918.0, "color": "#1aaf5d"},{"label":"14","value":14131.0, "color": "#1aaf5d"},{"label":"15","value":15435.0, "color": "#1aaf5d"},{"label":"16","value":16330.0, "color": "#1aaf5d"},{"label":"17","value":17360.0, "color": "#1aaf5d"},{"label":"18","value":14526.0, "color": "#1aaf5d"},{"label":"19","value":14943.0, "color": "#1aaf5d"},{"label":"20","value":13671.0, "color": "#1aaf5d"},{"label":"21","value":12764.0, "color": "#1aaf5d"},{"label":"22","value":11093.0, "color": "#1aaf5d"},{"label":"23","value":6573.0, "color": "#1aaf5d"},{"label":"24","value":5595.0, "color": "#1aaf5d"},{"label":"25","value":3189.0, "color": "#1aaf5d"},{"label":"26","value":1757.0, "color": "#1aaf5d"},{"label":"27","value":1031.0, "color": "#1aaf5d"},{"label":"28","value":729.0, "color": "#1aaf5d"},{"label":"29","value":255.0, "color": "#1aaf5d"},{"label":"30","value":258.0, "color": "#1aaf5d"},{"label":"31","value":129.0, "color": "#1aaf5d"},{"label":"32","value":6.0, "color": "#1aaf5d"},{"label":"33","value":0.0, "color": "#1aaf5d"},{"label":"34","value":6.0, "color": "#1aaf5d"},{"label":"35","value":27.0, "color": "#1aaf5d"},{"label":"42","value":14.0, "color": "#1aaf5d"}]
 
               }
           });
